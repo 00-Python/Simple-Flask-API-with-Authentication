f41f554dfcbb851f9276f7d92a01dfb4e6178556
Joseph Webster <rwc.webster@gmail.com>
2023-08-19 22:01:32 +0100

    Fixed Authentication

--- app.py
+++ app.py
@@ -30,15 +30,17 @@ def signup():
     return jsonify({"message": "User registered successfully"}), 201
 
 
-# Authentication endpoint
+# Authentication and token generation endpoint
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
 
-    if username in users and users[username] == password:
-        access_token = create_access_token(identity=username)
+    user = User.query.filter_by(username=username).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        access_token = create_access_token(identity=user.id)
         return jsonify({"access_token": access_token}), 200
     else:
         return jsonify({"message": "Invalid credentials"}), 401
